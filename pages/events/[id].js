ee25dd714aee85b5c2967fdc689faa3cf41aff92
Keerty Verma <keertyverma1303@gmail.com>
2023-06-01 13:24:40 +0530

    Show event page

--- pages/events/[id].js
+++ pages/events/[id].js
@@ -1,11 +1,33 @@
+import EventContent from "@/components/event-detail/event-content";
+import EventLogistics from "@/components/event-detail/event-logistics";
+import EventSummary from "@/components/event-detail/event-summary";
+import { getEventById } from "@/data/dummy-data";
 import { useRouter } from "next/router";
+import { Fragment } from "react";
 
 const EventDetailPage = () => {
   const router = useRouter();
+
+  const eventId = router.query.id;
+  const event = getEventById(eventId);
+
+  if (!event) {
+    return <p>No event found!</p>;
+  }
+
   return (
-    <div>
-      <h1>EventDetailPage - {router.query.id}</h1>
-    </div>
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
   );
 };
 
