87d4ad42c81bc68fb89b6bedaa6347697a0eb11e
Keerty Verma <keertyverma1303@gmail.com>
2023-06-02 18:46:56 +0530

    Optimize Image

--- components/events/EventItem.jsx
+++ components/events/EventItem.jsx
@@ -3,6 +3,7 @@ import Button from "../ui/Button";
 import DateIcon from "../icons/DateIcon";
 import AddressIcon from "../icons/AddressIcon";
 import ArrowRightIcon from "../icons/ArrowRightIcon";
+import Image from "next/image";
 
 const EventItem = ({ id, title, image, location, date }) => {
   const humanReadableDate = new Date(date).toLocaleDateString("en-US", {
@@ -15,7 +16,8 @@ const EventItem = ({ id, title, image, location, date }) => {
 
   return (
     <li className={classes.item}>
-      <img src={"/" + image} alt={title} />
+      <Image src={"/" + image} alt={title} width={250} height={160} />
+
       <div className={classes.content}>
         <div className={classes.summary}>
           <h2>{title}</h2>
--- pages/_app.js
+++ pages/_app.js
@@ -7,10 +7,12 @@ export default function App({ Component, pageProps }) {
   return (
     <Layout>
       <Head>
+        <title>Event Planner</title>
         <meta
           name="viewport"
           content="initial-scale=1.0, width=device-width"
         ></meta>
+        <meta name="description" content="Event planner"></meta>
       </Head>
       <Component {...pageProps} />
     </Layout>
